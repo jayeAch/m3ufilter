ac6f25ace837d157291d3b31221b960ca1227780
jayeAch <jhall_1974@hotmail.com>
2026-06-09 11:44:50 -0400

    update

--- generate-config.py
+++ generate-config.py
@@ -11,6 +11,15 @@ GLOBAL_EXCLUDE_LIST = [
 "б", "в", "г", "д", "ё", "ж", "з", "и", "й", "к", "л", "м", "н", "п", "т", "ф", "ц", "ч", "ш", "щ", "ъ", "ы", "ь", "э", "ю", "я", "Б", "В", "Г", "Д", "Ё", "Ж", "З", "И", "Й", "К", "Л", "М", "Н", "П", "Т", "Ф", "Ц", "Ч", "Ш", "Щ", "Ъ", "Ы", "Ь", "Э", "Ю", "Я",
 
 # All other entries (alphabetized, case-sensitive, deduplicated)
+"SEC Network",
+"Science HD",
+"LMN HD",
+"Big Ten Network",
+"ACC Network",
+"BOOT",
+"Local Now Charlotte",
+"Very Carolina by",
+#
 "Alaska State Troopers",
 "Joysauce",
 "I Shouldn't Be Alive",
