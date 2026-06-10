52cb07951339d2e677c4bffe99c67cf272afe1e1
jayeAch <jhall_1974@hotmail.com>
2026-06-10 17:31:53 -0400

    update

--- generate-config.py
+++ generate-config.py
@@ -11,6 +11,7 @@ GLOBAL_EXCLUDE_LIST = [
 "б", "в", "г", "д", "ё", "ж", "з", "и", "й", "к", "л", "м", "н", "п", "т", "ф", "ц", "ч", "ш", "щ", "ъ", "ы", "ь", "э", "ю", "я", "Б", "В", "Г", "Д", "Ё", "Ж", "З", "И", "Й", "К", "Л", "М", "Н", "П", "Т", "Ф", "Ц", "Ч", "Ш", "Щ", "Ъ", "Ы", "Ь", "Э", "Ю", "Я",
 
 # All other entries (alphabetized, case-sensitive, deduplicated)
+"Space City Home Network",
 "MSG",
 "Willow Cricket",
 "YES Network",
