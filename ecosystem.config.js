5659ad6bdd8a9718d0e6a4ff62e327349a263ca6
jayeAch <jhall_1974@hotmail.com>
2026-06-28 11:42:15 -0400

    update

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,13 +2,11 @@ module.exports = {
   apps: [{
     name: "m3u-filter",
     script: "dist/app-controller.js",
-    instances: "max",
-    exec_mode: "cluster",
+    instances: "1",
+    exec_mode: "fork",
     node_args: [
       "--max-old-space-size=768",
-      "--optimize-for-size", 
       "--max-semi-space-size=64",
-      "--turbo-filter=M3UFilterStream|handleGetm3u"
     ],
     env: {
       NODE_ENV: "production"
