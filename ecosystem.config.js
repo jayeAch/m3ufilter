53239bdea41aaf7f6c72959481ebbf37a866064e
jayeAch <jhall_1974@hotmail.com>
2026-06-30 12:33:41 -0400

    update

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,8 +2,14 @@ module.exports = {
   apps: [{
     name: "m3u-filter",
     script: "dist/app-controller.js",
+    out_file: "/dev/null",
+    error_file: "/dev/null",
     instances: "1",
     exec_mode: "fork",
+    watch: false,
+    autorestart: true,
+    restart_delay: 3000,
+    max_restarts: 10,
     node_args: [
       "--max-old-space-size=768",
       "--max-semi-space-size=64",
