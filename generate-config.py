3f1978eec8aea4504c7328915f0c85b7f8fb3b91
jayAch <jhall_1974@hotmail.com>
2026-07-31 22:08:02 -0400

    update

--- generate-config.py
+++ generate-config.py
@@ -11,6 +11,11 @@ GLOBAL_EXCLUDE_LIST = [
 "б", "в", "г", "д", "ё", "ж", "з", "и", "й", "к", "л", "м", "н", "п", "т", "ф", "ц", "ч", "ш", "щ", "ъ", "ы", "ь", "э", "ю", "я", "Б", "В", "Г", "Д", "Ё", "Ж", "З", "И", "Й", "К", "Л", "М", "Н", "П", "Т", "Ф", "Ц", "Ч", "Ш", "Щ", "Ъ", "Ы", "Ь", "Э", "Ю", "Я",
 
 # All other entries (alphabetized, case-sensitive, deduplicated)
+"Pluto TV Urbex",
+"Mera Mera TV",
+"Tarima TV",
+"El Señor de los Cielos",
+"ADN NOTICIAS",
 "Air Disaster",
 "Sky News",
 "The Therapy Crouch",
