5b8d35073c1849217d12723b618fa024f413b793
jayAch <jhall_1974@hotmail.com>
2026-08-05 22:09:01 -0400

    update

--- generate-config.py
+++ generate-config.py
@@ -11,6 +11,8 @@ GLOBAL_EXCLUDE_LIST = [
 "б", "в", "г", "д", "ё", "ж", "з", "и", "й", "к", "л", "м", "н", "п", "т", "ф", "ц", "ч", "ш", "щ", "ъ", "ы", "ь", "э", "ю", "я", "Б", "В", "Г", "Д", "Ё", "Ж", "З", "И", "Й", "К", "Л", "М", "Н", "П", "Т", "Ф", "Ц", "Ч", "Ш", "Щ", "Ъ", "Ы", "Ь", "Э", "Ю", "Я",
 
 # All other entries (alphabetized, case-sensitive, deduplicated)
+"Fox Deportes",
+"Fox Soccer",
 "TSN ",
 "Pluto TV Urbex",
 "Mera Mera TV",
